60c98ce4e9600d90ff976ae39542cfcd88286f5b
gheddie <stefan.schulz.1976@googlemail.com>
2026-02-26 11:51:08 +0100

    date bigfix

--- src/main/java/de/gravitex/banking_core/util/DateUtil.java
+++ src/main/java/de/gravitex/banking_core/util/DateUtil.java
@@ -1,5 +1,6 @@
 package de.gravitex.banking_core.util;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 
@@ -7,5 +8,9 @@ public class DateUtil {
 
 	public static long getMilliSeconds(LocalDate aLocalDate) {
 		return aLocalDate.atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli();
+	}
+
+	public static LocalDate getLocalDate(Long aMilliSeconds) {
+		return Instant.ofEpochMilli(aMilliSeconds).atZone(ZoneId.systemDefault()).toLocalDate();
 	}
 }
